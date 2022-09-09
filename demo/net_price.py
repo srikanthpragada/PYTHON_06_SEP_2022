2fb8e705519f42d0b9a806a58248c681fb20de5b
srikanthpragada <srikanthpragada@gmail.com>
2022-09-09 19:53:21 +0530

    09-SEP

--- demo/net_price.py
+++ demo/net_price.py
@@ -2,5 +2,7 @@
 
 price = int(input("Enter price :"))
 discount = price * 15 // 100
-net_price = price - discount
+after_discount_price = price - discount
+tax = after_discount_price * 8 // 100
+net_price = after_discount_price + tax
 print('Net Price = ', net_price)
---
+++ demo/show_result.py
@@ -0,0 +1,8 @@
+marks = int(input("Enter marks :"))
+
+if marks >= 50:
+    print("Passed")
+else:
+    print("Failed")
+
+print("End")
