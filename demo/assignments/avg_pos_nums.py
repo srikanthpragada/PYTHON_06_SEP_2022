101694b359d19074a2f83375ef66808c72f43f18
srikanthpragada <srikanthpragada@gmail.com>
2022-09-13 19:57:30 +0530

    13-SEP

---
+++ demo/assignments/avg_pos_nums.py
@@ -0,0 +1,15 @@
+
+total = count = 0
+
+while True:
+    num = int(input("Enter number [0 to stop] : "))
+    if num == 0:
+        break
+
+    if num < 0:
+        continue
+
+    total += num
+    count += 1
+
+print("Avg = ", total // count)
